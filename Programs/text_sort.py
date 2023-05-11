4f18b719ecbe2db6251fc4192a70c9c97b76e4f5
rutherfordium1 <128898461+rutherfordium1@users.noreply.github.com>
2023-05-11 10:32:31 +0800

    Delete Programs directory

--- Programs/text_sort.py
+++
@@ -1,8 +0,0 @@
-with open("unsorted.txt", "r") as f:
-    lines = f.readlines()
-
-sorted_lines = sorted(lines)
-
-with open("sorted.txt", "w") as f:
-    for line in sorted_lines:
-        f.write(line)
