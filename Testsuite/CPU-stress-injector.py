91d5dcbe48073c525077dc59dae5644105a4df92
rutherfordium1 <128898461+rutherfordium1@users.noreply.github.com>
2023-05-11 10:32:41 +0800

    Delete Testsuite directory

--- Testsuite/CPU-stress-injector.py
+++
@@ -1,5 +0,0 @@
-import stressinjector as injector
-
-
-if __name__ == '__main__':
-    injector.CPUStress(seconds=300)
--- Testsuite/memory-stress-injector.py
+++
@@ -1,5 +0,0 @@
-import stressinjector as injector
-
-
-if __name__ == '__main__':
-    injector.MemoryStress(gigabytes=2_000)
