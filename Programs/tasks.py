6128b78971f6edd6f506763505d7bb49ad05e243
rutherfordium1 <128898461+rutherfordium1@users.noreply.github.com>
2023-06-01 07:01:45 +0800

    Delete Programs directory

--- Programs/tasks.py
+++
@@ -1,7 +0,0 @@
-tasks = [
-    {"name": "Task1", "priority": 3, "type": "TypeA", "callout": "python script1.py"},
-    {"name": "Task2", "priority": 2, "type": "TypeB", "callout": "python script2.py"},
-    {"name": "Task3", "priority": 1, "type": "TypeA", "callout": "python script3.py"},
-    {"name": "Task4", "priority": 2, "type": "TypeC", "callout": "python script4.py"},
-    {"name": "Task5", "priority": 1, "type": "TypeB", "callout": "python script5.py"},
-]
